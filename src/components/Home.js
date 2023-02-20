1cf2176faf2abfaeb81867fe23c7ede54de608de
Robert <rlgallegos85@gmail.com>
2023-02-19 21:27:53 -0500

    JSX Lab

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,17 @@
 import React from "react";
 import { name, city } from "../data/data.js";
 
-function Home() {
-  // update the JSX being returned!
-  return <div>Home</div>;
+const divStyle = {
+  color: "firebrick"
 }
+
+function Home() {
+  return (
+  <div id="home">
+    <h1 style={divStyle} >{name} is a Web Developer from {city}</h1>
+  </div>
+);
+}
+
 
 export default Home;
